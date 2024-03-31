5626fb945bae07345d10b6683d3b47536290a35d
Mauricio Figueroa <maurixfx@gmail.com>
2024-03-31 22:40:13 +0200

    Project setup

---
+++ SplineVisionOS/SplineVisionOS/SplineVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineVisionOSApp.swift
+//  SplineVisionOS
+//
+//  Created by Mauricio Figueroa on 31-03-24.
+//
+
+import SwiftUI
+
+@main
+struct SplineVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
